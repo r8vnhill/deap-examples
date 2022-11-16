dbb9980b0c4671ee5421608b5795fdbc2325e3da
R8V <ignacio.slater@ug.uchile.cl>
2022-11-16 12:30:07 -0300

    Sync

--- genetic_algorithm/guess_word.py
+++ genetic_algorithm/guess_word.py
@@ -6,9 +6,11 @@
 import array
 import functools
 import random
+import re
 import string
 
-from deap import base, creator
+import numpy
+from deap import algorithms, base, creator, tools
 
 
 def get_optimizer() -> base.Fitness:
@@ -38,10 +40,68 @@ def attribute_generator(the_toolbox: base.Toolbox) -> functools.partial:
     The attribute generator.
     It chooses a random character from the string of ASCII characters.
     """
-    alphabet = string.ascii_letters
+    res = list(re.sub('.', lambda x: r'\u % 04X' % ord(x.group()), string.printable))
+
     try:
         the_toolbox.attr_bool
     except AttributeError:
-        the_toolbox.register("attr_bool", random.choice, alphabet)
+        the_toolbox.register("attr_bool", random.choice, res)
     return the_toolbox.attr_bool
 
+
+def individual_initializer(the_toolbox: base.Toolbox) -> functools.partial:
+    try:
+        the_toolbox.individual
+    except AttributeError:
+        the_toolbox.register("individual", tools.initRepeat, individual_creator(),
+                             attribute_generator(the_toolbox), 10)
+    return the_toolbox.individual
+
+
+def population_initializer(the_toolbox: base.Toolbox) -> functools.partial:
+    try:
+        the_toolbox.population
+    except AttributeError:
+        the_toolbox.register("population", tools.initRepeat, list,
+                             individual_initializer(the_toolbox))
+    return the_toolbox.population
+
+
+toolbox = base.Toolbox()
+
+target: str
+
+
+def count_matches(individual):
+    """
+    The fitness function.
+    It counts the number of characters that match the target word.
+    """
+    return sum(1 for expected, actual in zip(target, individual) if expected == actual),
+
+
+toolbox.register("evaluate", count_matches)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+
+def main():
+    pop = population_initializer(toolbox)(n=300)
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40,
+                                   stats=stats, halloffame=hof, verbose=True)
+
+    return pop, log, hof
+
+
+if __name__ == "__main__":
+    target = "Sopaipilla"
+    _, _, fittest = main()
+    print(fittest[0])
